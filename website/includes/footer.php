2ff00f604ea02ef7df030abb6b5d469f432c029e
Christine Pestana <clpestanaa@gmail.com>
2022-01-22 15:51:35 -0800

    commit changes

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -1,9 +1,13 @@
-</footer>
- 
-<script>
-     document.getElementById("html-checker").seaAttribute("href", "https://validator.w3.org/nu/?showsource=yes&showoutline=yes&showimagereport=yes&doc=https%3A%2F%2Fchristine-it-261-portal.herokuapp.com%2F" + location.href);
-     document.getElementById("css-checker").seaAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fchristine-it-261-portal.herokuapp.com%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" + location.href);
-   
-</script>
-</body>
-</html>
+<footer>
+        <div id="inner-footer">
+            <ul>
+                <li>Copyright &copy;
+                 <?php echo date('Y') ; ?>
+                </li>
+                <li>All Rights Reserved</li>
+                <li><a href="../index.php">Web Design by Christine</a></li>
+                <li><a href="https://validator.w3.org/nu/?showsource=yes&showoutline=yes&showimagereport=yes&doc=https%3A%2F%2Fchristine-it-261-portal.herokuapp.com%2F">HTML</a></li>
+                <li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fchristine-it-261-portal.herokuapp.com%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en">CSS</a></li>
+            </ul>
+        </div> <!--end inner-footer-->
+    </footer>
