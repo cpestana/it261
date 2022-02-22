b096ce70828915db9e36b30a07c9c86fabf5f60a
Christine Pestana <clpestanaa@gmail.com>
2022-02-22 14:13:23 -0800

    commit contact updates

--- website/contact.php
+++ website/contact.php
@@ -5,7 +5,7 @@
             <h1>Container Garden Plants</h1>
             <h3>Let us know what kind of plants you'd like for your container garden!</h3>
             <?php include 'includes/form.php';?>
-</div>
+    </div>
 
          
 
