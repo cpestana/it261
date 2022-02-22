9867e43dd3086b019e7837acfdcdf359f80d1de9
Christine Pestana <clpestanaa@gmail.com>
2022-02-22 14:08:49 -0800

    updates to css

--- website/contact.php
+++ website/contact.php
@@ -1,15 +1,14 @@
 <?php include 'includes/header.php'; ?>
 
-    <div id="wrapper">
-        <main>
+    <div id="contact_main">
+        
             <h1>Container Garden Plants</h1>
             <h3>Let us know what kind of plants you'd like for your container garden!</h3>
             <?php include 'includes/form.php';?>
-        </main>
 </div>
- 
-            
-           
-        
 
+         
+
+ 
+    
 <?php include 'includes/footer.php'; ?>
