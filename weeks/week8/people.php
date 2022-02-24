538210785989141d6350aacd18ca94a2ffb7d487
Christine Pestana <clpestanaa@gmail.com>
2022-02-23 19:18:04 -0800

    commit people.php

--- weeks/week8/people.php
+++ weeks/week8/people.php
@@ -5,7 +5,12 @@ include('./includes/header.php');
 
 <main>
 <h1>Welcome to my people page</h1>
+<?php
+$sql = 'SELECT * FROM people'; 
+//then we are going to connect to the database!
+$iConn = mysqli_connect()
 
+?>
 </main>
 
 <aside>
